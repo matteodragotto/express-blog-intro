add7836a791fb72784e5ae6c95cb3713e74dd0cc
matteodragotto <matteodrag8@gmail.com>
2024-12-17 13:59:43 +0100

    [feat] base express configuration

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Server del mio blog');  
+})
+
+app.listen(port, () => {
+  console.log('Sto ascoltando');
+  
+})
